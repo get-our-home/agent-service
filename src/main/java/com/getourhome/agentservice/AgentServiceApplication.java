ad0574177fe0792006051b98cb654f037e276e18
wonjun0120 <wonjundero@gmail.com>
2024-08-14 21:17:28 +0900

    feat: chart 업데이트 및 k8s 배포 테스트 완료
    #10

--- src/main/java/com/getourhome/agentservice/AgentServiceApplication.java
+++ src/main/java/com/getourhome/agentservice/AgentServiceApplication.java
@@ -2,12 +2,10 @@ package com.getourhome.agentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AgentServiceApplication {
 
     public static void main(String[] args) {
